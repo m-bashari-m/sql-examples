f937bd62ae04fd0977d0b50202d1df22de1fa8a6
m-bashari-m <89944686+m-bashari-m@users.noreply.github.com>
2023-12-06 14:03:32 +0330

    Create INSERT_INTO.sql

---
+++ INSERT_INTO.sql
@@ -0,0 +1,14 @@
+-- Can change order of giving values
+INSERT INTO Customers (CustomerName, ContactName, Address, City, PostalCode, Country)
+VALUES ('Cardinal', 'Tom B. Erichsen', 'Skagen 21', 'Stavanger', '4006', 'Norway');
+
+-- Or set Specific values
+INSERT INTO Customers (CustomerName, City, Country)
+VALUES ('Cardinal', 'Stavanger', 'Norway');
+
+-- Multiple rows
+INSERT INTO Customers (CustomerName, ContactName, Address, City, PostalCode, Country)
+VALUES
+('Cardinal', 'Tom B. Erichsen', 'Skagen 21', 'Stavanger', '4006', 'Norway'),
+('Greasy Burger', 'Per Olsen', 'Gateveien 15', 'Sandnes', '4306', 'Norway'),
+('Tasty Tee', 'Finn Egan', 'Streetroad 19B', 'Liverpool', 'L1 0AA', 'UK');
