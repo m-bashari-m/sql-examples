a2002a0650a6cf32af49a2b45db572604751db78
m-bashari-m <89944686+m-bashari-m@users.noreply.github.com>
2023-12-06 13:46:53 +0330

    Update SELECT.sql

--- SELECT.sql
+++ SELECT.sql
@@ -11,6 +11,22 @@ SELECT DISTINCT author_id as id FROM Views
 WHERE author_id = viewer_id
 ORDER BY id;
 
+-- Select Distinct Count
+SELECT COUNT(DISTINCT Country) FROM Customers;
+
 -- VARCHAR length
 SELECT tweet_id FROM Tweets
 WHERE LENGTH(content) > 15;
+
+-- Cities equal to "Paris" or "London"
+SELECT * FROM Customers
+WHERE City IN ('Paris','London');
+
+-- Price between range
+SELECT * FROM Products
+WHERE Price BETWEEN 50 AND 60;
+
+-- Search for pattern | s% -> Cities start with s
+-- %s% -> Cities include s
+SELECT * FROM Customers
+WHERE City LIKE 's%';
