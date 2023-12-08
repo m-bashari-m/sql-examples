3b7e801b10a0c169aca32cfd8f3a7dc3f9bb0fe4
m-bashari-m <89944686+m-bashari-m@users.noreply.github.com>
2023-12-09 00:02:17 +0330

    Update SELECT.sql

--- SELECT.sql
+++ SELECT.sql
@@ -14,6 +14,23 @@ ORDER BY id;
 -- Select Distinct Count
 SELECT COUNT(DISTINCT Country) FROM Customers;
 
+-- As keyword
+SELECT CustomerID AS ID
+FROM Customers; 
+
+-- As is optional so it's equal previous one
+SELECT CustomerID ID
+FROM Customers; 
+
+-- Concat names, so the result would be something like this:  	Obere Str. 57, 12209 Berlin, Germany
+SELECT CustomerName, Address + ', ' + PostalCode + ' ' + City + ', ' + Country AS Address
+FROM Customers;
+
+-- Prevent using redundant words
+SELECT o.OrderID, o.OrderDate, c.CustomerName
+FROM Customers AS c, Orders AS o
+WHERE c.CustomerName='Around the Horn' AND c.CustomerID=o.CustomerID;
+
 -- VARCHAR length
 SELECT tweet_id FROM Tweets
 WHERE LENGTH(content) > 15;
@@ -49,3 +66,17 @@ LIMIT 3;
 -- Order the return top 3
 SELECT TOP 3 * FROM Customers
 ORDER BY CustomerName DESC;
+
+-- Return all customers that have an order 
+SELECT * FROM Customers
+WHERE CustomerID IN (SELECT CustomerID FROM Orders);
+
+-- Return ProductName between these 2 given conditions
+-- Checks every single character
+SELECT * FROM Products
+WHERE ProductName BETWEEN "Carnarvon Tigers" AND "Chef Anton's Cajun Seasoning"
+ORDER BY ProductName;
+
+-- Between dates
+SELECT * FROM Orders
+WHERE OrderDate BETWEEN '1996-07-01' AND '1996-07-31';
